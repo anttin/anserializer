5691c729f1400d0a1c16014bdcebf80cee3350af
Antti Nilsson <muut.py@antion.fi>
2022-04-09 13:37:21 +0300

    pep8 formatting changes

--- anserializer/__init__.py
+++ anserializer/__init__.py
@@ -1,7 +1,6 @@
-name = "anserializer"
-
-from .serializer import Serializer
-from .serializers.datetimeserializer import DatetimeSerializer
-from .serializers.objectserializer import ObjectSerializer
-from .serializers.enumserializer import EnumSerializer
 from .serializers.baseserializer import BaseSerializer
+from .serializers.enumserializer import EnumSerializer
+from .serializers.objectserializer import ObjectSerializer
+from .serializers.datetimeserializer import DatetimeSerializer
+from .serializer import Serializer
+name = "anserializer"
